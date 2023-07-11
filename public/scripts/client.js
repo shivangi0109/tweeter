c5efdfa8e0c5a42b02ed263028ca71cb68d2b595
Shivangi Srivastava <shi06dec@gmail.com>
2023-07-11 16:53:57 -0600

    Implement loadTweets function to fetch tweets with AJAX & display the time using timeago library

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,36 +3,24 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
 $(document).ready(function() {
 
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function(tweets) {
+        renderTweets(tweets);
+      })
+      .catch(function(error) {
+        console.error('Error loading tweets:', error);
+      });
+  };
+
+  loadTweets(); // Load tweets on page load
 
   const renderTweets = function(tweets) {
+    // Clear existing tweets container
+    $('.tweets-container').empty();
+
     // loops through tweets
     for (let tweet of tweets) {
       // calls createTweetElement for each tweet
@@ -60,7 +48,7 @@ $(document).ready(function() {
         </div>
         <footer>
           <div>
-            <span>${tweet.created_at}</span>
+            <span>${timeago.format(tweet.created_at)}</span>
           </div>
           <div>
             <i class="fas fa-flag"></i>
@@ -73,8 +61,6 @@ $(document).ready(function() {
 
     return $tweet;
   }
-
-  renderTweets(data);
 
   const $form = $('form'); // Get the form element
   
